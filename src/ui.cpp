5f21aab46400a2b8e2bfeb0c334b3e9212cd11e7
robob27 <robertgoodberry@robertgoodberry.com>
2024-09-01 22:55:54 -0400

    Fix bad optional access error

--- src/ui.cpp
+++ src/ui.cpp
@@ -132,8 +132,12 @@ void uilist_impl::draw_controls()
                             parent.hovered = parent.fentries[ i ];
                         }
                         ImGui::SameLine( 0, 0 );
-                        cataimgui::draw_colored_text( entry.hotkey.value().short_description(),
-                                                      is_selected ? parent.hilight_color : parent.hotkey_color );
+
+                        if (entry.hotkey.has_value()) {
+                            nc_color color = is_selected ? parent.hilight_color : parent.hotkey_color;
+                            cataimgui::draw_colored_text(entry.hotkey.value().short_description(),
+                                color);
+                        }
 
                         ImGui::TableSetColumnIndex( 1 );
                         nc_color color = ( is_selected ?
